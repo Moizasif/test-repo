d59aef6f5bee49c1d16accf0319cfc74e51213e1
Moizasif <42133954+Moizasif@users.noreply.github.com>
2023-03-03 15:51:00 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var http = require('http');
+var server = http.createServer(function (request, response) {  response.writeHead(200, {"Content-Type": "text/plain"});
+response.end("Hello World\n");});
+server.listen(80);
+console.log("Server running at http://127.0.0.1:80/");
