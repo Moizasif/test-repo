8f8deceaf063840b4510aa4aa60088a4c19353d7
Moizasif <42133954+Moizasif@users.noreply.github.com>
2023-03-09 11:23:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var http = require('http');
-http.createServer(function (req, res) {  
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.end('Hello World!');
-}).listen(8080);
+// var http = require('http');
+// http.createServer(function (req, res) {  
+//  res.writeHead(200, {'Content-Type': 'text/html'});
+//  res.end('Hello World!');
+// }).listen(8080);
+
+var msg = 'Hello World';
+console.log(msg);
